33ae41f706423ab3f2a2c0a3cde0ea2a679b1e61
Dmytruk Serhii <dmitruksl92@gmail.com>
2023-05-01 21:13:56 +0300

    Adding Router and new component

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,19 +1,23 @@
-const Pet = ({name, animal, breed, location, images, id}) => {
+import { Link } from "react-router-dom";
+
+const Pet = ({ name, animal, breed, location, images, id }) => {
   let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
-  if(images.length) {
+  if (images.length) {
     hero = images[0];
   }
 
   return (
-  <a href={`/details/${id}`} className="pet">
-    <div className="image-container">
-      <img src={hero} alt={name} />
-    </div>
-    <div className="info">
-      <h1>{name}</h1>
-      <h2>{animal} - {breed} - {location}</h2>
-    </div>
-  </a>
+    <Link to={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>
+          {animal} - {breed} - {location}
+        </h2>
+      </div>
+    </Link>
   );
 };
 
